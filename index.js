22c6d74ba858bed35551e9bebe8c082badaecc9f
dfergusbrown <dfergusbrown@gmail.com>
2024-04-01 19:03:52 -0400

    bottles render according to number in server response

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
 /* --- CACHED ELEMENTS --- */
 const sign = document.querySelector('#sign')
+const container = document.querySelector(".container")
+const theWall = document.querySelector('#theWall')
+const bottleTemplate = document.querySelector('#bottle')
+
+/* --- VARIABLES --- */
+const url = 'http://localhost:3000'
 
 /* --- FUNCTIONS --- */
+fetchBottles()
+async function fetchBottles() {
+    const response = await fetch(`${url}/`, {
+        mode: 'cors',
+        headers: {
+          'Access-Control-Allow-Origin':'*'
+        }
+      })
+      jsonObj = await response.json()
+      number = jsonObj[0].number
+    setSign(number)
+    setBottles(number)
+}
 
+function setSign(num) {
+    sign.textContent = `${num} Bottles Saloon`
+}
+
+function setBottles(num) {
+    for (let i = 0; i < num; i++) {
+        const bottleClone = bottleTemplate.cloneNode(true)
+        bottleClone.classList.remove('hide')
+        const bottleDiv = document.createElement('div')
+        bottleDiv.appendChild(bottleClone)
+        bottleDiv.classList.add('bottle')
+        theWall.appendChild(bottleDiv)
+    }
+}
